04b3402514069c4bf008e70d4250f8be8d20f7df
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-08-04 21:39:31 +0200

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadlineComponent } from './headline/headline.component';
 import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadlineComponent,
-    HeaderComponent
+    HeaderComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
